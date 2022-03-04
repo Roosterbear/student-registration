aeb284d7d5f77811e29c043012b064500b1e0cce
Roosterbear <lfpereag@gmail.com>
2022-03-04 10:51:37 -0600

    Updating

--- application/views/introConfigVw.php
+++ application/views/introConfigVw.php
@@ -2,12 +2,11 @@
     <form action="http://sito-misc.app.utags.edu.mx/reinscripcion/index.php/DataConfiguration" method="post">
         <label for="acceso">
             <h2 class="conf"><i class="fa fa-eye"></i> Escribe tu acceso al sistema</h2>
-        </label>     
-        <input type="password" id="acceso" name="acceso" value=""  class="form__input" />      
+        </label>
+        <input type="password" id="acceso" name="acceso" value=""  class="form__input" />
         <div class="espacio"></div>
         <div id="div--btn__acceso">
             <button id = "btn_acceso" type="submit" class="btn btn__verde">Ingresar</button>
         </div>
     </form>
 </div>
-
